eecdd8fe668273f34bfd4baee9fdd9c89647db2f
henry <henryonyemaobi@gmail.com>
2020-07-18 08:57:22 +0100

    changed fav icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,9 @@
 
         <title>Watch Dog</title>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/fav/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/images/fav/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/images/fav/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/fav2/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/fav2/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/fav2/favicon-16x16.png">
 <!--<link rel="manifest" href="/images/fav/site.webmanifest">-->
 
    <!-- Web Application Manifest -->
